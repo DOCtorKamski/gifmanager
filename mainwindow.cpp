59925f780a0473e4af048945b47989d3e892303c
DOCtorKamski <kamski@myarch.localdomain>
2025-12-10 20:12:34 +0500

    add ability to add multiple GIFs in GridLayout, ugly but works

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,15 +10,38 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     //TODO change how find path
-    movie = new QMovie("/path/to/my_file.gif");
-    ui->label->setMovie(movie);
-    ui->label->show();
+    QStringList gif_files = {
+        "/path/to/my_file1.gif"
+        "/path/to/my_file2.gif"
+        "/path/to/my_file3.gif"
+        "/path/to/my_file4.gif"
+        "/path/to/my_file5.gif"
+    };
 
-    movie->start();
+    //TODO move this
+    int row_count{};
+    int col_count{};
 
-    //resize label to gif size TODO make like method
-    QSize gif_size = movie->currentImage().size();
-    ui->label->setFixedSize(gif_size);
+    for(const QString &gif_path : gif_files) {
+        QLabel *label = new QLabel();
+        QMovie *movie = new QMovie(gif_path);
+        label->setMovie(movie);
+        movie->start();
+
+        //resize label to gif size TODO make like method
+        QSize gif_size = movie->currentImage().size();
+        label->setFixedSize(gif_size);
+
+        // add label to grid layout
+        ui->gridLayout->addWidget(label, row_count, col_count);
+
+        // update row and column index
+        col_count++;
+        if (col_count >= 3) { // TODO add to change to desired number of columns
+            col_count = 0;
+            row_count++;
+        }
+    }
 }
 
 MainWindow::~MainWindow()
